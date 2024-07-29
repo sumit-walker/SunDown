f30bdc8173053b2705617885304cc91e9adb315d
sumit-walker <117709430+sumit-walker@users.noreply.github.com>
2024-07-29 14:40:52 +0530

    Update app.js
    commit edit

--- sundown_project/Sundown/app.js
+++ sundown_project/Sundown/app.js
@@ -102,52 +102,5 @@ function swiper(){
 
 
 
-
-
-
-
-function LocomotiveGsap(){
-console.clear();
-
-gsap.registerPlugin(ScrollTrigger);
-
-
-
-//I facing some scrolling issue while using Gsap and locomotive  together that's why i used this code from github. this code resolve my scolling issue.
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-locoScroll.on("scroll", ScrollTrigger.update);
-
-
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
-
-
-
-
-
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-}
-
-// locomotiv()
-
-LocomotiveGsap()
-
 swiper();
+locomotiv()
